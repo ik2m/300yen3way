b28b0b11119bb460a825b76a54d8d1b22c7d6fbf
ikezaki <izumiikezaki@users.noreply.github.com>
2019-04-07 20:01:49 +0900

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var random;
+var text;
+$(function(){
+
+    $('#stop-btn').on('click',function() {
+        random=Math.random();
+        console.log(random);
+        text=setTextByRandNum(random);
+        $('#roulette-text').text(text);
+    })
+    
+    function setTextByRandNum(randNum) {
+        if(randNum<0.5){
+            return "おかし";
+        }else if(randNum<0.8){
+            return "ジュース";
+        }else{
+            return "お茶";
+        }
+    }
+});
